b660187a4e8d8bec0e9982ca35410154ccda2679
Quammy93 <Quammy93@gmail.com>
2023-08-03 21:16:25 +0000

    variable annotation

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''python type-annotated function to_str that takes a float n
+'''
+
+
+def to_str(n: float) -> str:
+
+    '''
+    returning float
+
+    '''
+
+    return str(n)
