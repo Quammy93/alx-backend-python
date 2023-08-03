773b4444f79c91785b1ec939310289b046563539
Quammy93 <Quammy93@gmail.com>
2023-08-03 22:45:15 +0000

    variable-annotation

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
-
-'''
-type-annotated function sum_list which takes a list input_list
-
-'''
-
+"""
+a function that type-annotated sum_list
+"""
 from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     return a sum of all nums inside a list
-
     """
     return sum(input_list)
